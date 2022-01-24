34fb0cf9946dcad5e7551533518ce1f7edf90add
MaryahKim <maryapitaz@gmail.com>
2022-01-24 17:56:22 +0300

    Create Modal.vue
    create a modal file in components

---
+++ src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+             <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+   export default {
+       props: ['header', 'text', 'theme']
+   }
+</script>
+
+<style scoped>
+
+.modal {
+    width: 400px;
+    padding: 20px;
+     margin: 100px auto;
+    background: whitesmoke;
+    border-radius: 10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+p{
+    color: white;
+}
+h1{
+    color: violet;
+    padding: 0;
+    border: none;
+}
+
+ .sale{
+    background: crimson;
+    color: white;
+}
+
+ .sale h1{
+   color: white; 
+}
+</style>
